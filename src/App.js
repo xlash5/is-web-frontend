7f1f2e7cd8e5a8c95f996e845ec44fd7a87a6b6f
Enes <menesilhan@gmail.com>
2022-06-03 20:54:36 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/Login";
+import { HomePage } from "./pages/Home";
+import { ProfilePage } from "./pages/Profile";
+import { SettingsPage } from "./pages/Settings";
+import { ProtectedLayout } from "./components/ProtectedLayout";
+import { HomeLayout } from "./components/HomeLayout";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<HomeLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+
+      <Route path="/dashboard" element={<ProtectedLayout />}>
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="settings" element={<SettingsPage />} />
+      </Route>
+    </Routes>
+  );
+}
